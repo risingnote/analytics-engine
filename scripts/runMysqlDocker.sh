8915af5f1a4fa31c0ae0f4b092ebc2f3a7158b34
Jonathan Evans <jon.evans@bristol.ac.uk>
2017-08-01 13:53:39 +0100

    Tidy start scripts.

--- scripts/runMysqlDocker.sh
+++ scripts/runMysqlDocker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# run from home directory
+HERE=$(cd `dirname $0`; pwd)
+cd $HERE/..
 
 docker run -d --rm --name mysqlDev -v $(pwd)/datadir:/var/lib/mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes mysql:5.7
 
