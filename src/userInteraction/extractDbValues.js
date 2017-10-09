6003fc30440e0696d0351362712663e1c9e07d0c
Jonathan Evans <jon.evans@bristol.ac.uk>
2017-10-09 13:40:33 +0100

    Add file back.

---
+++ src/userInteraction/extractDbValues.js
@@ -0,0 +1,36 @@
+const db = require('../db')
+const spdz = require('../spdz')
+
+/**
+ * Extract the data and validate sizes against the analytic function. 
+ * @param {String} query as SQL to run 
+ * @param {Object} analyticFunc to apply to query data 
+ * 
+ * @returns {Array<Array>} array of input data arrays to support batching inputs
+ */
+const extractDbValues = (query, analyticFunc) => {
+  return db
+    .runQuery(query)
+    .then(data => {
+      try {
+        spdz.verifyQuery(Object.keys(data[0]).length, data.length, analyticFunc)
+        return data
+      } catch (err) {
+        return Promise.reject(err)
+      }
+    })
+    .then(data => {
+      // Extract values from each row and flatten into 1d array (row1col1, row1col2, row2col1 etc.)
+      const colKeys = Object.keys(data[0])
+      const matrixData = data.map(row => colKeys.map(key => row[key]))
+      const arrayData = [].concat.apply([], matrixData)
+
+      return spdz.formatInput(
+        arrayData,
+        colKeys.length,
+        analyticFunc.inputRowCount.rowBufferSize
+      )
+    })
+}
+
+module.exports = extractDbValues
